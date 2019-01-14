3088838fcd3fb72ceedcacb13bf5a96dedb1c31c
STeven <sanguine.steve@gmail.com>
2019-01-14 16:02:06 +0200

    Side nav done. TODO: Cleanup

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,16 @@
 $( document ).ready(function() {
-	
 
 	
 });
 
 
+	
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
